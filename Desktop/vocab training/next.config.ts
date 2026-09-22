2dcc8999b7bd4eca104efcc9c0ed05d6112de865
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2026-09-22 21:41:42 +0200

    Update next.config.ts

--- Desktop/vocab training/next.config.ts
+++ Desktop/vocab training/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/lexicon",
+  assetPrefix: "/lexicon/",
   turbopack: {
     root: path.resolve(__dirname),
   },
